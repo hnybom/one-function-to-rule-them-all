747d2bb6e91834d612b9b3c7c48910094d1cdf4f
Henri Nybom <Henri.Nybom@solita.fi>
2015-09-07 07:15:25 +0300

    extra task missing

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -20,31 +20,57 @@
   (reduce (fn [acc e] (inc acc)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [acc e] (conj acc e)) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [mm e] (if (empty? mm)
+                       [e e]
+                       (let [min-v (first mm)
+                             max-v (second mm)]
+                         (cond
+                           (< e min-v) [e max-v]
+                           (> e max-v) [min-v e]
+                           :else [min-v max-v]))))
+          [] a-seq))
 
 (defn insert [sorted-seq n]
-  [:-])
+    (loop [loop-seq sorted-seq
+           new-seq []]
+      (let [current-e (first loop-seq)]
+        (cond
+          (empty? loop-seq) (conj new-seq n)
+          (< n current-e) (concat (conj new-seq n) loop-seq)
+          :else (recur (rest loop-seq) (conj new-seq current-e))))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
+
+(defn toggle [a-set elem]
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn parity [a-seq]
-  [:-])
+  (reduce toggle #{} a-seq))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& more]
+  (count more))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([] 1)
+  ([x] x)
+  ([x y] (* x y))
+  ([x y & more] (reduce my-* (my-* x y) more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] (fn [val] true))
+  ([x] x)
+  ([x y] (fn [val] (and (x val) (y val) y)))
+  ([x y & more] (reduce pred-and (pred-and x y) more)))
 
 (defn my-map [f a-seq]
   [:-])
